4536b847da5687583be05f76f3e13dbe60a76a88
rdsubhas <rdsubhas@gmail.com>
2014-11-17 19:01:38 +0530

    Upgrade to express 4.x, add gzip, redirect herokuapp to www.devopsbookmarks.com, references #17

--- server.js
+++ server.js
@@ -6,33 +6,18 @@ if (cluster.isMaster && process.env.NODE_ENV) {
     cluster.fork();
 } else {
   var express = require('express');
-  var http = require('http');
-  var path = require('path');
-
   var app = express();
 
-  // all environments
-  app.set('port', process.env.PORT || 3000);
-  app.set('views', path.join(__dirname, 'views'));
+  app.set('views', __dirname + '/views');
   app.set('view engine', 'ejs');
+  app.use(require('connect-redirecthost').redirectHost('www.devopsbookmarks.com'));
+  app.use(require('morgan')('combined'));
+  app.use(require('compression')());
+  app.use(require('errorhandler')());
 
-  app.use(express.favicon());
-  app.use(express.json());
-  app.use(express.urlencoded());
-  app.use(express.methodOverride());
-  app.use(app.router);
-  app.use(express.static(path.join(__dirname, 'public')));
-
-  // development only
-  if ('development' == app.get('env')) {
-    app.use(express.logger('dev'));
-    app.use(express.errorHandler());
-  }
-
+  app.use(express.static(__dirname + '/public'));
   app.get('/', require('./routes/home'));
   app.get(/^\/([a-zA-Z0-9+-]+)$/, require('./routes/tools'));
 
-  http.createServer(app).listen(app.get('port'), function(){
-    console.log('Express server listening on port ' + app.get('port'));
-  });
+  app.listen(process.env.PORT || 3000);
 }
