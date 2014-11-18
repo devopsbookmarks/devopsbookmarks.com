20c488867a03d786655ff391cc279c01d7a31023
rdsubhas <rdsubhas@gmail.com>
2014-11-18 19:22:17 +0530

    Separate licenses/platforms/topics into separate files

--- data/index.js
+++ data/index.js
@@ -1,11 +1,15 @@
 var _u = require('underscore');
 var walk = require('walk');
 var fs = require('fs');
+
+var topics = JSON.parse(fs.readFileSync(__dirname + '/topics.json'));
+var platforms = JSON.parse(fs.readFileSync(__dirname + '/platforms.json'));
+var licenses = JSON.parse(fs.readFileSync(__dirname + '/licenses.json'));
 var tools = [];
 
-exports.topics = _u.indexBy(require('./tags').topics, 'slug');
-exports.platforms = _u.indexBy(require('./tags').platforms, 'slug');
-exports.licenses = _u.indexBy(require('./tags').licenses, 'slug');
+exports.topics = _u.indexBy(topics, 'slug');
+exports.platforms = _u.indexBy(platforms, 'slug');
+exports.licenses = _u.indexBy(licenses, 'slug');
 exports.tools = tools;
 
 walker = walk.walk(__dirname + '/tools/');
@@ -15,6 +19,7 @@ walker.on('file', function(root, fileStats, next) {
     var toolStr = fs.readFileSync(root + '/' + fileStats.name, 'utf-8');
     var toolJson = JSON.parse(toolStr);
     toolJson.forEach(function(tool) {
+      tools['topics'] = '...'
       tools.push(tool);
     });
   }
