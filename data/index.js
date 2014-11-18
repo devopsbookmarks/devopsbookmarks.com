4d8a6b3866c4e7ebb0de1978cc012a5933437a91
rdsubhas <rdsubhas@gmail.com>
2014-11-18 18:50:50 +0530

    Separate files for tools

--- data/index.js
+++ data/index.js
@@ -1,6 +1,27 @@
 var _u = require('underscore');
+var walk = require('walk');
+var fs = require('fs');
+var tools = [];
 
 exports.categories = _u.indexBy(require('./tags').categories, 'slug');
 exports.platforms = _u.indexBy(require('./tags').platforms, 'slug');
 exports.licenses = _u.indexBy(require('./tags').licenses, 'slug');
-exports.tools = require('./tools').tools;
+exports.tools = tools;
+
+walker = walk.walk(__dirname + '/tools/');
+
+walker.on('file', function(root, fileStats, next) {
+  if (fileStats.name.match(/.json$/)) {
+    var toolStr = fs.readFileSync(root + '/' + fileStats.name, 'utf-8');
+    var toolJson = JSON.parse(toolStr);
+    toolJson.forEach(function(tool) {
+      tools.push(tool);
+    });
+  }
+
+  next();
+});
+
+walker.on('end', function() {
+  console.log('Read ' + tools.length + ' tools');
+});
