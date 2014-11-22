f2342516dacf85f26dde7d8b38b6bcc354a2f8fa
rdsubhas <rdsubhas@gmail.com>
2014-11-22 13:21:09 +0530

    Add HTTP caching for CSS, references #15

--- server.js
+++ server.js
@@ -8,15 +8,19 @@ if (cluster.isMaster) {
   var express = require('express');
   var app = express();
   var port = process.env.PORT || 3000;
+  var staticAsset = require('static-asset');
 
   app.set('views', __dirname + '/views');
   app.set('view engine', 'ejs');
+
   app.use(require('connect-redirecthost').redirectHost('www.devopsbookmarks.com'));
   app.use(require('morgan')('combined'));
   app.use(require('compression')());
   app.use(require('errorhandler')());
 
+  app.use(staticAsset(__dirname + '/public'));
   app.use(express.static(__dirname + '/public'));
+
   app.get('/', require('./routes/home'));
   app.get('/:tags', require('./routes/tools'));
 
